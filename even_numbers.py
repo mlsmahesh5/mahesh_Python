48be0f274c150761756d22795734079614f1c0bb
mlsmahesh5 <mlsmahesh5@gmail.com>
2022-10-28 00:28:35 +0530

    changes made in local

--- even_numbers.py
+++
@@ -1,8 +0,0 @@
-# Print even numbers from the range values entered by the user
-
-start_value = int(input("Enter Start range value: "))
-end_value = int(input("Enter Start range value: "))
-
-for x in range(start_value, end_value ):
-    if x%2 == 0:
-        print(x, end=" ")
